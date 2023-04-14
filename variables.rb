6754647b03ca6e439b4aab274cc25638b3c26e38
Derek Chavez <129697286+DChavez18@users.noreply.github.com>
2023-04-13 23:13:42 -0600

    Update variables.rb

--- variables.rb
+++ variables.rb
@@ -1,8 +1,8 @@
 name = "derek"
 email = "frrenose17@yahoo.com"
 
-name # "dominic"
-email # "dmchavez1824@yahoo.com"
+name = "dominic"
+email = "dmchavez1824@yahoo.com"
 
 age = 32
 pets = 3
@@ -10,7 +10,7 @@ pets = 3
 hunger_level = 9.9
 tired_level = 5.5
 
-married = true
+is_married = true
 single = false
 
 puts "Hello World"
